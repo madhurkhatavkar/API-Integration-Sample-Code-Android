4bbdb70bd83ba6b430f974e2eb0d913566f4d363
Madhur Khatavkar <77504674+madhurkhatavkar@users.noreply.github.com>
2021-08-09 11:45:29 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -15,37 +15,223 @@
  * limitations under the License.
  */
 
-package com.smsAlert;  /* import sms alert package  */
+package com.smsAlert;  /* import sms alert api package  */
 
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-import com.smsAlert.R;
-import com.smsAlert.SMSALERT;     /* import sms alert api  */
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by Cozy Vision Pvt. Ltd.
+ */
+
+public class SMSALERT extends AsyncTask<String, Void, String> {
+
+    String mainUrl = "https://www.smsalert.co.in/api/push.json?";
+    String otpUrl = "https://www.smsalert.co.in/api/mverify.json?";
+    String apikey, user, pwd, auth, text, sender, mobileno, msgSchedule, route, code;
+    boolean isFlash = false, isUnicode = false, debugMode = false;
+    String debugTAG = "SMSALERT";
+    String response;
+    String template = "Hello, Your OTP is [otp]";
+
+    //api
+    public SMSALERT(String key) {
+        apikey = key;
+    }
+
+	//api debug
+    public SMSALERT(String key, boolean debug) {
+        apikey = key;
+        debugMode = debug;
+    }
+
+    //Authentication using username and password
+    public SMSALERT(String username,String password) {
+       user = username;
+       pwd = password;
+    }
 
 
-public class MainActivity extends AppCompatActivity {
-    TextView text;
+	//compose message
+    public void composeMessage(String id, String message) {
+        sender = id;
+        text = message;
+    }
+
+    //compose mobile
+    public void to(String mobile) {
+        mobileno = mobile;
+    }
+   
+    //flas if needed
+    public void flash(boolean trueORfalse) {
+        isFlash = trueORfalse;
+    }
+
+	//unicode if needed
+    public void unicode(boolean trueORfalse) {
+        isUnicode = trueORfalse;
+    }
+
+	//set route
+    public void setRoute(String route) {
+        route = route;
+    }
+
+    public void setauth(){
+        if (apikey != null)
+            auth ="&apikey=" + apikey;
+        else
+            auth ="&user=" + user+ "&pwd=" + pwd;
+    }
+
+    //get xml string
+    public String getXML() throws UnsupportedEncodingException{
+        setauth();
+
+        String finalLink = mainUrl + auth + "&sender=" + sender + "&mobileno=" + mobileno + "&text=" + URLEncoder.encode(text, "UTF-8");
+        
+        return finalLink;
+    }
+
+    //get xml string
+    public String getotpXML() throws UnsupportedEncodingException{
+        setauth();
+
+        String finalLink = otpUrl + auth + "&sender=" + sender + "&mobileno=" + mobileno + "&template=" + URLEncoder.encode(template, "UTF-8");
+
+        return finalLink;
+    }
+
+    //get xml string
+    public String setotpXML() throws UnsupportedEncodingException{
+        setauth();
+
+        String finalLink = otpUrl + auth + "&mobileno=" + mobileno + "&code=" + code;
+
+        return finalLink;
+    }
+
+	//send function
+    public String send() {
+        try {
+            return execute("SEND").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return "Exception: " + e;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return "Exception: " + e;
+        }
+    }
+
+    //send otp function
+    public String sendOTP() {
+        try {
+            return execute("SENDOTP").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return "Exception: " + e;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return "Exception: " + e;
+        }
+    }
+
+    //verify otp function
+    public String verifyOTP(String code) {
+        try {
+            return execute("VERIFYOTP").get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return "Exception: " + e;
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            return "Exception: " + e;
+        }
+    }
+	
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-		
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        text = (TextView) findViewById(R.id.text);
+    protected String doInBackground(String... params) {
+        try {
 
-        /* Create SMS ALERT object with your Auth Key */
-        SMSALERT msgSmsAlert = new SMSALERT("your_auth_key");
+            URLConnection myURLConnection;
+            URL myURL = null;
+            BufferedReader reader;
 
-		// compose message with senderid and text-message
-        msgSmsAlert.composeMessage("SENDERID", "This Sample message body that will be sent with sender id : SENDERID");
 
-        // .to(String) : will send message to single mobile number
-        msgSmsAlert.to("9718603323";
+            String command = params[0];
+            switch (command) {
+                case "SEND":
+                    String url = getXML();
+                    if (!url.contains("ERROR")) {
+                        myURL = new URL(url);
+                    } else {
+                        return url;
+                    }
+                    break;
 
-        // NOTE : Before calling this function you MUST call .composeMessage & .to
-        String sendStatus = msgSmsAlert.send();
-        text.setText("Send Status : " + sendStatus);
-      
+                case "SENDOTP":
+                    String sendotp = getotpXML();
+                    if (!sendotp.contains("ERROR")) {
+                        myURL = new URL(sendotp);
+                    } else {
+                        return sendotp;
+                    }
+                    break;
+
+                case "VERIFYOTP":
+                    String verifyotp = setotpXML();
+                    if (!verifyotp.contains("ERROR")) {
+                        myURL = new URL(verifyotp);
+                    } else {
+                        return verifyotp;
+                    }
+                    break;
+            }
+
+            if (myURL != null) {
+                if (debugMode)
+                    Log.d(debugTAG, "URL: " + myURL.toString());
+                myURLConnection = myURL.openConnection();
+                myURLConnection.connect();
+                reader = new BufferedReader(new InputStreamReader(myURLConnection.getInputStream()));
+                String lineReader;
+                while ((lineReader = reader.readLine()) != null)
+                    response = lineReader;
+                if (debugMode)
+                    Log.d(debugTAG, "RESPONSE: " + response);
+                reader.close();
+
+                if (response.contains("error")) {
+                    if (response.contains("error"))
+                        return "Message not send.";
+                    else if (response.contains("success"))
+                        return "Message sent successfully";
+                    else
+                        return response;
+                }
+            } else
+                response = "ERROR (NULL) : URL is NULL";
+
+            return response;
+
+        } catch (IOException e) {
+            if (debugMode)
+                return "ERROR (IOException) : Unable to download the requested page.\n";
+            else
+                return "ERROR (IOException) : \n" + e;
+        }
     }
 }
